6d89b5e9a09cf582ef423bb5b1b3fceee28b1e48
rayypdm <demesaarianneray@gmail.com>
2025-01-20 00:28:44 +0800

    added files

---
+++ JavaScript/script.js
@@ -0,0 +1,11 @@
+let bbname = "Woonagi";
+console.log("Hello World");
+console.log(bbname);
+alert("Skrrr~ " + bbname);
+document.getElementById("sometext").innerHTML = bbname;
+
+bbname = "Snowhak";
+alert("Skrrr~ " + bbname);
+
+
+
